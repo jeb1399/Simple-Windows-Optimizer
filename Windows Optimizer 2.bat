b7733fb1fc0e33af15cb00e658ebeac27f758261
takenusername <145628605+jeb1399@users.noreply.github.com>
2025-07-09 18:45:00 -0400

    Update Windows Optimizer 2.bat

--- Windows Optimizer 2.bat
+++ Windows Optimizer 2.bat
@@ -1,7 +1,8 @@
+@echo off
+
 REM This is version 2 it has a little more I might not make v3 but idk
 
 TITLE Windows Optimizer - Version 2
-@echo off
 setlocal enabledelayedexpansion
 net session >nul 2>&1
 if %errorLevel% neq 0 (
